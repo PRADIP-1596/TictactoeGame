0a62626de25b5a7b897340e600d1c9467047ae77
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-06-12 08:13:43 +0530

    -Show_Board

--- TictacToe.sh
+++ TictacToe.sh
@@ -1,32 +1,32 @@
 
-#!/bin/bash
+#!/bin/bash -x
 
 declare -A board
-board_Size=3
+BOARD_ROWS=3
+BOARD_SIZE=$(($BOARD_ROWS*$BOARD_ROWS))
 USER_SIGN=0
 COMP_SIGN=0
 
-function initialize_Board()
-for (( row=1; row<4 ; row++ )) do
-      for (( column=1; column<4 ; column++ )) do
-#         read var
-			board[$row,$column]=0
-      done
-done
+function initialize_Board(){
+	for (( key=1; key<=$BOARD_SIZE ; key++ )) do
+      		board[$key]=0
+      	done
+}
 
 function show_Board(){
 
-	for (( row=1; row<4 ; row++ )) do
-   	for (( column=1; column<4 ; column++ )) do
-			if [ ${board[$row,$column]}==0 ]
-         then
-            printf  _" "
+	for (( count=1; count<$BOARD_SIZE ; count++ )) do
+		if [ ${board[$count]} -eq 0 ]
+         	then
+			printf  _" "
         	else
-            printf ${board[$row,$column]}" "
-         fi
-		done
-   echo
-done
+			printf ${board[$count]}" "
+         	fi
+		if [ $(( $count % $BOARD_ROWS )) -eq 0 ]
+		then
+			echo
+		fi
+	done
 }
 
 function toss_Assign_Sign(){
@@ -43,15 +43,28 @@ function toss_Assign_Sign(){
 function toss_Plays_First(){
 	randomVariable=$((RANDOM%2))
 	if [ $randomVariable -eq 0 ]
-   then
-      echo Computer plays first
-   else
-      You play first
-   fi
+	then
+		echo Computer plays first
+	else
+		echo You play first
+   	fi
 }
 
+function take_User_Input(){
+	read -p "Please enter the number between 1-9 where insert your $USER_SIGN in board" input;
+	echo $input "hi";
+
+        if [  $input -gt 0  -a $input -le 9 ]
+	then
+		echo valid input
+	else
+		echo invalid input
+	fi
+}
 
 initialize_Board
 toss_Assign_Sign
 toss_Plays_First
 show_Board
+take_User_Input
+echo ${board[@]}
