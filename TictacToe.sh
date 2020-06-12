34b191777b7689c1aa394c2a5eabc371284c1df2
PRADIP-1596 <9187pradipyadav@gmail.com>
2020-06-12 19:43:52 +0530

    Computer_plays_like _me

--- TictacToe.sh
+++ TictacToe.sh
@@ -1,22 +1,24 @@
-
 #!/bin/bash -x
 
 declare -A board
 BOARD_ROWS=3
 BOARD_SIZE=$(($BOARD_ROWS*$BOARD_ROWS))
-USER_SIGN=0
-COMP_SIGN=0
+USER_SIGN="O"
+COMP_SIGN="O"
+stop=false
+valid=false
 
 function initialize_Board(){
+	local key=0
 	for (( key=1; key<=$BOARD_SIZE ; key++ )) do
       		board[$key]=0
       	done
 }
 
 function show_Board(){
-
-	for (( count=1; count<$BOARD_SIZE ; count++ )) do
-		if [ ${board[$count]} -eq 0 ]
+	local count=0
+	for (( count=1; count<=$BOARD_SIZE ; count++ )) do
+		if [ "${board[$count]}" == "0" ]
          	then
 			printf  _" "
         	else
@@ -33,9 +35,9 @@ function toss_Assign_Sign(){
 	randomVariable=$((RANDOM%2))
 	if [ $randomVariable -eq 0 ]
 	then
-		USER_SIGN=X
+		USER_SIGN="X"
 	else
-		COMP_SIGN=X
+		COMP_SIGN="X"
 	fi
 	echo "Your sign is "$USER_SIGN" and computer sign is "$COMP_SIGN
 }
@@ -45,26 +47,168 @@ function toss_Plays_First(){
 	if [ $randomVariable -eq 0 ]
 	then
 		echo Computer plays first
+		first=user
 	else
 		echo You play first
+		first=comp
    	fi
 }
 
-function take_User_Input(){
-	read -p "Please enter the number between 1-9 where insert your $USER_SIGN in board" input;
-	echo $input "hi";
-
-        if [  $input -gt 0  -a $input -le 9 ]
+function check_Valid(){
+	if [ $1 -gt 0  -a $1 -le 9 ]
 	then
-		echo valid input
+		valid=true
+	fi
+	if [ "$valid" == "true" -a "${board[$1]}" == "0" ]
+        then
+        	board[$1]=$2
 	else
-		echo invalid input
+		valid=false
+        fi
+
+}
+
+function comuter_Plays(){
+	while [ "$valid" == "false" ]
+        do
+		number=$((RANDOM%9+1))
+		check_Valid $number $COMP_SIGN
+	done
+	valid=false
+}
+
+function take_User_Input(){
+	while [ "$valid" == "false" ]
+	do
+		read -p "Please enter the number between 1-9 where insert your $USER_SIGN in board " input;
+		check_Valid $input $USER_SIGN
+		if [ "$valid" == "false" ]
+		then
+			echo Input not accepted please try again
+		fi
+	done
+	valid=false
+}
+
+function send_var(){
+	echo $1
+	wins $1
+}
+
+function wins(){
+	if [ $1 == $USER_SIGN ]
+	then
+		echo "You won"
+	else
+		echo "Computer Wins"
 	fi
 }
 
-initialize_Board
-toss_Assign_Sign
-toss_Plays_First
-show_Board
-take_User_Input
-echo ${board[@]}
+function search_Daigonal_Left_Right(){
+	local count=0
+	local increase_by=$((BOARD_ROWS+1))
+	for (( daig_Key=1; daig_Key <= $BOARD_SIZE; daig_Key+=$increase_by )) do
+		if [ ${board[$daig_Key]} == $1 ]
+		then
+			((count++))
+		fi
+	done
+	if [ $count -eq $BOARD_ROWS ]
+	then
+		wins $1
+		stop=true
+	fi
+}
+
+function search_Daigonal_Right_Left(){
+        local count=0
+        local increase_by=$((BOARD_ROWS-1))
+	local left_Bottom_Limit=$((BOARD_SIZE-BOARD_ROWS+1))
+        for (( daig_Key=$BOARD_ROWS; daig_Key <= $left_Bottom_Limit; daig_Key+=$increase_by )) do
+                if [ ${board[$daig_Key]} == $1 ]
+                then
+                        ((count++))
+                fi
+        done
+        if [ $count == $BOARD_ROWS ]
+        then
+                wins $1
+		stop=true
+        fi
+}
+
+function search_Rows(){
+	local count=0
+	key=0
+	for (( row=0;row<$BOARD_ROWS;row++ )) do
+		count=0
+		for (( col=1; col<=$BOARD_ROWS; col++ )) do
+			key=$(($BOARD_ROWS*row+col ))
+			if [ ${board[$key]} == $1 ]
+			then
+				(( count++ ))
+			fi
+		done
+		if [ $count -eq $BOARD_ROWS ]
+		then
+			wins $1
+			break
+		fi
+	done
+	if [ $count -eq $BOARD_ROWS ]
+	then
+		stop=true
+	fi
+}
+
+function search_Columns(){
+        local count=0
+        key=0
+        for (( col=1;col<=$BOARD_ROWS;col++ )) do
+                count=0
+                for (( row=0; row<=$BOARD_ROWS; row++ )) do
+                        key=$(($BOARD_ROWS*row+col ))
+                        if [ "${board[$key]}" == "$1" ]
+                        then
+                                (( count++ ))
+                        fi
+                done
+                if [ $count -eq $BOARD_ROWS ]
+                then
+                        wins $1
+                        break
+
+		fi
+        done
+        if [ $count -eq $BOARD_ROWS ]
+        then
+                stop=true
+	fi
+}
+
+function check_Win(){
+	search_Daigonal_Left_Right $1
+	search_Daigonal_Right_Left $1
+	search_Rows $1
+	search_Columns $1
+}
+
+function play(){
+	initialize_Board
+	toss_Assign_Sign
+	toss_Plays_First
+	while [ $stop == false ]
+	do
+		valid=false
+		show_Board
+		take_User_Input
+		valid=false
+#		echo ${board[@]}
+		check_Win $USER_SIGN
+		comuter_Plays
+		check_Win $COMP_SIGN
+	done
+	show_Board
+}
+
+play
